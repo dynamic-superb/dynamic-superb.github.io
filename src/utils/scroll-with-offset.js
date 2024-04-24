cb8e0e551c816a81293eb52371ad87b60a06e03c
Kai Wei <ga642381@gmail.com>
2024-04-24 16:00:40 +0800

    Add build files for gh-pages

--- src/utils/scroll-with-offset.js
+++
@@ -1,7 +0,0 @@
-const scrollWithOffset = (el) => {
-    const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-    const yOffset = -70; 
-    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
-}
-
-export { scrollWithOffset };
